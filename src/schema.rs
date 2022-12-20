42144fdc9b4a6993c0883b7e5696676497c2301f
Abdulqudduus <babalolajnr@gmail.com>
2022-12-20 17:43:09 +0100

    feat: :sparkles: add todo module

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,18 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    todo (id) {
+        id -> Int4,
+        title -> Text,
+        description -> Text,
+        done -> Bool,
+        user_id -> Uuid,
+        created_at -> Timestamp,
+        updated_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
     user (id) {
         id -> Uuid,
         name -> Text,
@@ -10,3 +22,10 @@ diesel::table! {
         updated_at -> Nullable<Timestamp>,
     }
 }
+
+diesel::joinable!(todo -> user (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    todo,
+    user,
+);
